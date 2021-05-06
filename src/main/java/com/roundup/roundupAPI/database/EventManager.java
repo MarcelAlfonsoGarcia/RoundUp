7af7144d3b0078e9760d5fa1cada14162cfaa041
MarcelAlfonsoGarcia <magb2017@mymail.pomona.edu>
2021-05-06 15:42:18 -0400

    Finished setting up event manager

--- src/main/java/com/roundup/roundupAPI/database/EventManager.java
+++ src/main/java/com/roundup/roundupAPI/database/EventManager.java
@@ -1,6 +1,7 @@
 package com.roundup.roundupAPI.database;
 
 import java.sql.Timestamp;
+import java.time.LocalTime;
 
 /**
  * This class is intended to manage the status of events automatically. This
@@ -12,9 +13,12 @@ import java.sql.Timestamp;
  * 
  * This class will stand away from the usual components as it does not require
  * client interactions to follow nor does it intend to produce any outputs.
- * Ideally, it will run every day at a preset time to ensure that the events are
+ * Ideally, it is set to run every day at a preset time to ensure that the events are
  * updated as fast as possible but within the constraints of how much processing
  * power we are allowed to use under a free Heroku plan.
+ * 
+ * DISCLAIMER: To have this class meet its requirements, we would need to create a separate heroku
+ * application for it. As such, this is simply a proof of concept and code showcase.
  */
 public class EventManager {
 
@@ -37,21 +41,28 @@ public class EventManager {
 	 *         in the upcoming week (from the present date and time) to upcoming by
 	 *         calling updateEventStatus from the DAL
 	 */
-	private void updateAllEventsStatus() {
-        
-		// this time corresponds to Jan 1st 1970
-		Timestamp distantPast = new Timestamp(10000);
-		
-		// this time corresponds to the present time
-		Timestamp current = new Timestamp(System.currentTimeMillis());
-		
-		// this time corresponds to a week from right now
-		// the number 604800000 is the amount of milliseconds in a week
-		Timestamp future = new Timestamp(System.currentTimeMillis() + 604800000);
+	private static void updateAllEventsStatus() {
+		DAL dal = DAL.getInstance();
 
-		DAL.getInstance().updateEventStatus("active", current, future);
+		while (true) {
+			if (LocalTime.now().equals(LocalTime.of(6, 0, 0))) {
+				// this time corresponds to Jan 1st 1970
+				Timestamp distantPast = new Timestamp(10000);
 
-		DAL.getInstance().updateEventStatus("inactive", distantPast, current);
+				// this time corresponds to the present time
+				Timestamp current = new Timestamp(System.currentTimeMillis());
+
+				// this time corresponds to a week from right now
+				// the number 604800000 is the amount of milliseconds in a week
+				Timestamp future = new Timestamp(System.currentTimeMillis() + 604800000);
+
+				dal.updateEventStatus("active", current, future);
+				dal.updateEventStatus("inactive", distantPast, current);
+			}
+		}
 	}
 
+//	public static void main(String[] args) {
+//		updateAllEventsStatus();
+//	}
 }
